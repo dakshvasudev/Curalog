5257c4ee744eeb9cd5975f4b7345f87b0a04af4a
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2024-03-29 13:33:49 +0530

    Refactor BottomNavBar widget and update navigation bar style

--- lib/main.dart
+++ lib/main.dart
@@ -39,9 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return const AdaptivePageScaffold(
-      title: Text('Curalog'),
-      body: BottomNavBar()
-    );
+        title: Text('Curalog'), body: BottomNavBar());
   }
 }
 
@@ -51,40 +49,39 @@ class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Widget> buildScreens() {
-        return [
-          // Replace with the actual widgets for your pages ------------------------------------------------------------------------------------------------
-          const Center(child: Text('Home Page')),
-          const Center(child: Text('Second Page')),
-          const Center(child: Text('Third Page')),
+      return [
+        // TODO : Replace with the actual widgets for your pages
+        const Center(child: Text('Home Page')),
+        const Center(child: Text('Second Page')),
+        const Center(child: Text('Third Page')),
 
-          // HomeScreen(), 
-          // SecondScreen(), 
-          // ThirdScreen(),
-        ];
+        // HomeScreen(),
+        // SecondScreen(),
+        // ThirdScreen(),
+      ];
     }
 
     List<PersistentBottomNavBarItem> navBarsItems() {
-        return [
-            PersistentBottomNavBarItem(
-                icon: const Icon(Icons.home),
-                title: ("Home"),
-                activeColorPrimary: Colors.blue,
-                inactiveColorPrimary: Colors.grey,
-            ),
-            PersistentBottomNavBarItem(
-                icon: const Icon(Icons.settings),
-                title: ("Settings"),
-                activeColorPrimary: Colors.blue,
-                inactiveColorPrimary: Colors.grey,
-            ),
-            PersistentBottomNavBarItem(
-                icon: const Icon(Icons.person),
-                title: ("Profile"),
-                activeColorPrimary: Colors.blue,
-                inactiveColorPrimary: Colors.grey,
-            ),
-            
-        ];
+      return [
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.home),
+          title: ("Home"),
+          activeColorPrimary: colors(context).highlight.s500,
+          inactiveColorPrimary: colors(context).onSurface.s500,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.settings),
+          title: ("Settings"),
+          activeColorPrimary: colors(context).highlight.s500,
+          inactiveColorPrimary: colors(context).onSurface.s500,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.person),
+          title: ("Profile"),
+          activeColorPrimary: colors(context).highlight.s500,
+          inactiveColorPrimary: colors(context).onSurface.s500,
+        ),
+      ];
     }
 
     PersistentTabController controller;
@@ -92,34 +89,39 @@ class BottomNavBar extends StatelessWidget {
     controller = PersistentTabController(initialIndex: 0);
 
     return PersistentTabView(
-        context,
-        controller: controller,
-        screens: buildScreens(),
-        items: navBarsItems(),
-        confineInSafeArea: true,
-        backgroundColor: Colors.white, // Default is Colors.white.
-        handleAndroidBackButtonPress: true, // Default is true.
-        resizeToAvoidBottomInset: true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-        stateManagement: true, // Default is true.
-        hideNavigationBarWhenKeyboardShows: true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-        decoration: NavBarDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          colorBehindNavBar: Colors.white,
-        ),
-        popAllScreensOnTapOfSelectedTab: true,
-        popActionScreens: PopActionScreensType.all,
-        itemAnimationProperties: const ItemAnimationProperties( // Navigation Bar's items animation properties.
-          duration: Duration(milliseconds: 200),
-          curve: Curves.ease,
-        ),
-        screenTransitionAnimation: const ScreenTransitionAnimation( // Screen transition animation on change of selected tab.
-          animateTabTransition: true,
-          curve: Curves.ease,
-          duration: Duration(milliseconds: 200),
-        ),
-        navBarStyle: NavBarStyle.style1, // Choose the nav bar style with this property. ----------------------------------------------------------------
-        //there are many style given in the documentation https://pub.dev/packages/persistent_bottom_nav_bar change the .style1 according to your needs
-        //for example style15, style16 etc.
+      context,
+      controller: controller,
+      screens: buildScreens(),
+      items: navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: const ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: const ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle: NavBarStyle
+          .style1, // Choose the nav bar style with this property. ----------------------------------------------------------------
+      //there are many style given in the documentation https://pub.dev/packages/persistent_bottom_nav_bar change the .style1 according to your needs
+      //for example style15, style16 etc.
     );
   }
 }
